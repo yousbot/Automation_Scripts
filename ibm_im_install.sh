15876860b1dfaa3595a99d457fb81aded965f0ca
yousbot <sbaiidrissiyoussef@gmail.com>
2025-01-06 15:00:00 +0100

    updated

--- ibm_im_install.sh
+++ ibm_im_install.sh
@@ -2,14 +2,14 @@
 
 ## This script aims to install the Installation Manager
 
-################        ENVIRONMENT VARIABLES           ################
+############        ENVIRONMENT VARIABLES           ############
 
 media_full_path=/tmp/agent.installer.lnx.gtk.x86_64_1.8.5.zip
 extraction_full_path=/tmp/IM
 shared_path=/opt/IBM/Shared
 install_path=/opt/IBM/IM
 
-#########################################################################
+#################################################################
 
 echo " "; echo " ";
 
